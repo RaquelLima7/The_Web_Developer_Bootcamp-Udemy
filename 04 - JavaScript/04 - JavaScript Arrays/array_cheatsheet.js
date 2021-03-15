4998dc42c2ca0302a941b92341ad3156fa3dc90f
Raquel Lima <raqueljones182@gmail.com>
2021-03-15 07:18:23 -0300

    Concat, indexOf, includes & reverse

--- 04 - JavaScript/04 - JavaScript Arrays/array_cheatsheet.js
+++ 04 - JavaScript/04 - JavaScript Arrays/array_cheatsheet.js
@@ -33,3 +33,42 @@ movieLine = ["Jerry", "Denis", "Tom", "Nancy", "Oliver", "Eva"];
 movieLine = ["Tom", "Nancy", "Oliver", "Eva"];
 movieLine.shift()  // It will remove the first item
 movieLine = ["Nancy", "Oliver", "Eva"]; // removes "Tom"
+
+// concat() - merge two or more arrays
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+const array3 = array1.concat(array2); // ["a", "b", "c", "d", "e", "f"]
+
+// includes() - method determines whether an array includes a certain value among its entries
+const array1 = [1, 2, 3];
+
+console.log(array1.includes(2)); // expected output: true
+
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat')); // expected output: true
+
+console.log(pets.includes('at')); // expected output: false
+
+// indexOf() - returns the first index at which a given element can be found in the array, or -1 if it is not present.
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(beasts.indexOf('bison')); // expected output: 1
+
+// start from index 2
+console.log(beasts.indexOf('bison', 2)); // expected output: 4
+
+console.log(beasts.indexOf('giraffe')); // expected output: -1
+
+
+// reverse() - reverses an array, the first array element becomes the last, and the last array element becomes the first.
+const array1 = ['one', 'two', 'three'];
+console.log('array1:', array1); // expected output: "array1:" Array ["one", "two", "three"]
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed); // expected output: "reversed:" Array ["three", "two", "one"]
+
+// Careful: reverse is destructive -- it changes the original array.
+console.log('array1:', array1); // expected output: "array1:" Array ["three", "two", "one"]
+
+
