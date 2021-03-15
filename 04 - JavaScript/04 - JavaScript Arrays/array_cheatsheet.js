09d43a0dc1adaba0043c76c6d8795470a4bbb8e6
Raquel Lima <raqueljones182@gmail.com>
2021-03-15 06:21:39 -0300

    Modifying Arrays

--- 04 - JavaScript/04 - JavaScript Arrays/array_cheatsheet.js
+++ 04 - JavaScript/04 - JavaScript Arrays/array_cheatsheet.js
@@ -3,9 +3,13 @@ const colors = ["red", "orange", "yellow"];
 
 // Arrays are indexed like strings:
 colors[0]; // "red"
+colors[0][1] // "e"
 
 // They have a length:
 colors.length; //3
+
+// Modifying Arrays
+colors[0] = "pink" // just set it equal to another color
 
 // Important array methods:
 //push(value) - adds value to the END of an array
