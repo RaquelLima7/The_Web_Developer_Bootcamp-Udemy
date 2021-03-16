edbd2d3658d9e90be77b442577126e0c3e1f56a3
Raquel Lima <raqueljones182@gmail.com>
2021-03-15 21:03:30 -0300

    iterating objects

---
+++ 04 - JavaScript/06 - Repeating Stuff With Loops/IteratingObjects/app.js
@@ -0,0 +1,34 @@
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60
+}
+// ===================================
+// USING FOR...IN TO ITERATE AN OBJECT
+// ===================================
+
+for (let person in testScores) {
+    console.log(`${person} scored ${testScores[person]}`);
+}
+// Object.keys(testScores) // will return all keys inside the object
+// Object.entries(testScores) // will return all keys with values
+
+// ==============================================
+// Using Object methods to iterate
+// (turn data into an array and then use for...of)
+// ==============================================
+
+let total = 0;
+let scores = Object.values(testScores);
+for (let score of scores) {
+    total += score;
+}
+console.log(total / scores.length)
+
