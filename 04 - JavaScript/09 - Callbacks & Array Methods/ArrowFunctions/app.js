51ed1a5fe301077e31c98c2b809cf88f643d76a5
Raquel Lima <raqueljones182@gmail.com>
2021-03-17 09:02:31 -0300

    arrow

---
+++ 04 - JavaScript/09 - Callbacks & Array Methods/ArrowFunctions/app.js
@@ -0,0 +1,60 @@
+// const add = function(x,y) {
+//     return x + y;
+// }
+
+// const add = (x, y) => {
+//     return x + y;
+// }
+
+
+const add = (a, b) => a + b;
+
+// If have just one parameter can put without ()
+const square = num => {
+    return num * num;
+}
+
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1
+// }
+
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+
+
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+// const newMovies = movies.map(function (movie) {
+//     return `${movie.title} - ${movie.score / 10}`
+// })
+
+
+// IMPLICIT RETURN
+const newMovies = movies.map(movie => (
+    `${movie.title} - ${movie.score / 10}`
+))
+
+
+
+
+
