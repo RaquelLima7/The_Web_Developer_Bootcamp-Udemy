4dc7a5e8ec2195275772b38af6f45760790b5cb4
Raquel Lima <raqueljones182@gmail.com>
2021-03-23 07:45:01 -0300

    call stack

---
+++ 04 - JavaScript/14 - Async JavaScript/Callstack/app.js
@@ -0,0 +1,18 @@
+const multiply = (x, y) => x * y;
+
+//square calls multiply (that is a simple function)
+const square = x => multiply(x, x);
+
+
+//the isRightTriangle calls square 3 times
+//the isRightTriangle waits square and square waits multiply, and when get it the number, 
+//multiply is removed form call stack and square has a value and so on
+const isRightTriangle = (a, b, c) => (
+    square(a) + square(b) === square(c)
+)
+console.log("BEFORE")
+isRightTriangle(3, 4, 5)
+
+console.log("DONEEEE!")
+
+//explain Debug in sources
