aa0494bdaa6da8111815b2ceedf91fc3366faf87
Raquel Lima <raqueljones182@gmail.com>
2021-04-08 14:10:30 -0300

    intro mongo

---
+++ 08 - Mongo/mongo.js
@@ -0,0 +1,18 @@
+// Mongo - database
+
+// SQL - pre-define a schema of tables before we insert anything.
+// MySQL; Postgres; SQLite; Oracle; Microsoft SQL Server
+
+//NO-SQL DATABASES - don't use schema, use many types of no-sql databases, 
+// including document, key-value, and graph stores.
+// MongoDB; Couch DB; Neo4j; Cassandra; Redis
+
+// Run Mongo in terminal: just type mongo
+
+// db => in terminal is a data base using for default
+
+// show all data bases
+// show databases or show dbs // obs: only shows if inside of database has anything
+
+// create:
+// use nomeDataBase
