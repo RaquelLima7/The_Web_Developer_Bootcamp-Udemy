b189f3d06d62581ee4d5a011c6e3a387334a9ff7
Raquel Lima <raqueljones182@gmail.com>
2021-04-12 21:27:36 -0300

    comments

--- 08 - Mongo/MongooseBasics/index.js
+++ 08 - Mongo/MongooseBasics/index.js
@@ -1,3 +1,7 @@
+// mongoose => conect mongo and js
+// terminal: npm init -y
+// nmp i mongoose
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -8,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
         console.log(err)
     })
 
+//inform what tipy is each element
 const movieSchema = new mongoose.Schema({
     title: String,
     year: Number,
@@ -15,7 +20,8 @@ const movieSchema = new mongoose.Schema({
     rating: String
 });
 
-const Movie = mongoose.model('Movie', movieSchema);
+// Movie is a class and the first element 'Movie' must be singular and capitalize
+const Movie = mongoose.model('Movie', movieSchema); 
 // const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
 
 
